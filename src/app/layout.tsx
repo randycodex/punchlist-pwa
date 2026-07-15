3eb5d607a871feab4dda6ca5380ae08cba3fce0a
randy <randyrubirosa@gmail.com>
2026-07-15 15:58:42 -0400

    Enable iPhone safe area theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: "device-width",
+  viewportFit: "cover",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
